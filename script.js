c58d888203afe6038d5cee36689ada9b9b925e9d
Ariel Leyva <aleyva1509@gmail.com>
2022-12-02 01:23:41 -0600

    hours value also pads now

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ function update() {
     var currentTime = new Date()
     var hours = currentTime.getHours()
     var minutes = currentTime.getMinutes()
+    if (hours < 10){
+        hours = "0" + hours 
+    }
     if (minutes < 10){
         minutes = "0" + minutes
     }
